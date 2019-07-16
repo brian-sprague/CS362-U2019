ada9d3a169c567b89b06053f2ad0f6989a32e5f6
Brian Sprague <spragubr@oregonstate.edu>
2019-07-16 15:04:44 -0700

    Started on unittest2

---
+++ dominion/unittest2.c
@@ -0,0 +1,17 @@
+/********************************************************************
+* Author: Brian Sprague
+* Assignment 3
+* unittest1.c
+********************************************************************/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+
+int main ()
+{
+    return 0;
+}
