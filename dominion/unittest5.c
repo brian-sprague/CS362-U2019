77cb08d4d336a8cfc0cf011f82513c126cd21927
Brian Sprague <spragubr@oregonstate.edu>
2019-07-17 16:52:14 -0700

    Created unittest5 and added to makefile

---
+++ dominion/unittest5.c
@@ -0,0 +1,25 @@
+/********************************************************************
+* Author: Brian Sprague
+* Assignment 3
+* unittest5.c
+********************************************************************/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+
+int main ()
+{
+    int i;
+    int numPlayers = 2;
+    int kCards[10] = {adventurer, council_room, feast, gardens, mine,
+                 remodel, smithy, village, baron, great_hall};
+    struct gameState G;
+    int seed = time(0);
+
+
+    return 0;
+}
