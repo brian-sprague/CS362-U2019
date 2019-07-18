ce19d6b21703755b098e9f74d12bec9d5ad8d31f
Brian Sprague <spragubr@oregonstate.edu>
2019-07-17 21:43:44 -0700

    Created unittest4 and added to makefile

---
+++ dominion/cardtest4.c
@@ -0,0 +1,140 @@
+/********************************************************************
+* Author: Brian Sprague
+* Assignment 3
+* cardtest3.c
+********************************************************************/
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <time.h>
+
+int main ()
+{
+    int i;
+    int numPlayers = 2;
+    int kCards[10] = {adventurer, council_room, feast, gardens, mine,
+                 remodel, smithy, village, baron, great_hall};
+    struct gameState G;
+    int seed = time(0);
+    int players[MAX_PLAYERS];
+
+    // First player wins outright
+    initializeGame(numPlayers, kCards, seed, &G);
+
+    G.hand[0][0] = province;
+    G.hand[0][1] = province;
+    G.hand[0][2] = province;
+    G.hand[0][3] = province;
+    G.hand[0][4] = province;
+    G.handCount[0] = 5;
+
+    G.deck[0][0] = province;
+    G.deck[0][1] = province;
+    G.deck[0][2] = province;
+    G.deck[0][3] = province;
+    G.deck[0][4] = province;
+    G.deckCount[0] = 5;
+
+    G.discard[0][0] = province;
+    G.discard[0][1] = province;
+    G.discard[0][2] = province;
+    G.discard[0][3] = province;
+    G.discard[0][4] = province;
+    G.discardCount[0] = 5;
+
+    G.hand[1][0] = estate;
+    G.hand[1][1] = estate;
+    G.hand[1][2] = estate;
+    G.hand[1][3] = estate;
+    G.hand[1][4] = estate;
+    G.handCount[1] = 5;
+
+    G.deck[1][0] = estate;
+    G.deck[1][1] = estate;
+    G.deck[1][2] = estate;
+    G.deck[1][3] = estate;
+    G.deck[1][4] = estate;
+    G.deckCount[1] = 5;
+
+    G.discard[1][0] = estate;
+    G.discard[1][1] = estate;
+    G.discard[1][2] = estate;
+    G.discard[1][3] = estate;
+    G.discard[1][4] = estate;
+    G.discardCount[1] = 5;
+
+    getWinners(players, &G);
+
+    for (i = 0; i < MAX_PLAYERS; i++)
+    {
+        if (players[i] == 1)
+        {
+            printf("Player %d wins!\n", (i + 1));
+        }
+    }
+
+    assert(players[0] == 1);
+    memset(&G, '\0', sizeof(struct gameState));
+
+    // Second player wins due to not having as many turns
+    initializeGame(numPlayers, kCards, seed, &G);
+
+    G.hand[0][0] = province;
+    G.hand[0][1] = province;
+    G.hand[0][2] = province;
+    G.hand[0][3] = province;
+    G.hand[0][4] = province;
+    G.handCount[0] = 5;
+
+    G.deck[0][0] = province;
+    G.deck[0][1] = province;
+    G.deck[0][2] = province;
+    G.deck[0][3] = province;
+    G.deck[0][4] = province;
+    G.deckCount[0] = 5;
+
+    G.discard[0][0] = province;
+    G.discard[0][1] = province;
+    G.discard[0][2] = province;
+    G.discard[0][3] = province;
+    G.discard[0][4] = province;
+    G.discardCount[0] = 5;
+
+    G.hand[1][0] = province;
+    G.hand[1][1] = province;
+    G.hand[1][2] = province;
+    G.hand[1][3] = province;
+    G.hand[1][4] = province;
+    G.handCount[1] = 5;
+
+    G.deck[1][0] = province;
+    G.deck[1][1] = province;
+    G.deck[1][2] = province;
+    G.deck[1][3] = province;
+    G.deck[1][4] = province;
+    G.deckCount[1] = 5;
+
+    G.discard[1][0] = province;
+    G.discard[1][1] = province;
+    G.discard[1][2] = province;
+    G.discard[1][3] = province;
+    G.discard[1][4] = province;
+    G.discardCount[1] = 5;
+
+    getWinners(players, &G);
+
+    for (i = 0; i < MAX_PLAYERS; i++)
+    {
+        if (players[i] == 1)
+        {
+            printf("Player %d wins!\n", (i + 1));
+        }
+    }
+
+    assert(players[1] == 1);
+    
+    return 0;
+}
