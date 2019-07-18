da752b5c98956f8c0d0a5ea0025f6fa12bddfdc5
Brian Sprague <spragubr@oregonstate.edu>
2019-07-17 17:08:17 -0700

    First test of unittest5

--- dominion/unittest5.c
+++ dominion/unittest5.c
@@ -20,6 +20,19 @@ int main ()
     struct gameState G;
     int seed = time(0);
 
+    // init a new game
+    initializeGame(numPlayers, kCards, seed, &G);
+    G.whoseTurn = 0;
+    G.hand[0][0] = mine;
+    G.hand[0][0] = copper;
+    G.handCount[0] = 2;
+
+    playCard(0, G.hand[0][1], (G.hand[0][1] + 1), 0, &G);
+
+    printf("Card in player's hand: %d\n", G.hand[0][0]);
+    // assert(G.hand[0][0] == 5);
+    printf("Number of cards in player's hand: %d\n", G.handCount[0]);
+    // assert(G.handCount[0] == 1);
 
     return 0;
 }
