4c1aae892c7cacae79ab385d687341db88e848b8
Brian Sprague <spragubr@oregonstate.edu>
2019-07-17 19:34:02 -0700

    Error fix

--- dominion/cardtest3.c
+++ dominion/cardtest3.c
@@ -14,26 +14,24 @@
 int main ()
 {
     int i, j;
-    int numPlayers;
     int kCards[10] = {adventurer, council_room, feast, gardens, mine,
                  remodel, smithy, village, baron, great_hall};
     struct gameState G;
     int seed = time(0);
-    int refDeck[MAX_DECK];
-    int sameCards = 0;
+
 
     for (i = 2; i <= MAX_PLAYERS; i++)
     {
-        initializeGame(numPlayers, kCards, seed, &G);
+        initializeGame(i, kCards, seed, &G);
 
-        for (j = 0; j < numPlayers; j++)
+        for (j = 0; j < i; j++)
         {
-            print(" Current player: %d\n", (j + 1));
+            printf(" Current player: %d\n", (j + 1));
             assert(G.whoseTurn == j);
             endTurn(&G);
         }
 
-        print(" Current player: %d\n", (j + 1));
+        printf(" Current player: %d\n", (j + 1));
         assert(G.whoseTurn == j);
 
         memset(&G, '\0', sizeof(struct gameState));
